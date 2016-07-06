d820054b2018f07d20457b23e941d8d06f64e50d
treylitefm <griffin.omar@gmail.com>
2016-07-06 16:57:35 -0400

    Add model for redis CRUD
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

---
+++ flask/model.py
@@ -0,0 +1,35 @@
+from redis import Redis
+import json
+import os, time
+
+class Model:
+    '''Handle redis schema and CRUD'''
+    config = None
+    conn = None
+
+    def __init__(self):
+        with open('config.json', 'r') as f:
+            self.config = json.load(f)
+
+        self.conn = Redis(self.config['redis']['host']) 
+        os.environ['TZ'] = 'EST'
+        time.tzset()
+
+    def get_dl_url(self, url):
+        return self.conn.hgetall('downloads:'+url)
+
+    def has_already_been_downloaded(self, url):
+        result = self.get_dl_url(url)
+
+        if result is not None and bool(result['finished']) is True:
+            return True
+        return False
+
+    def insert_dl_url(self, url):
+        return self.conn.hmset('downloads:'+url, {'started': True, 'finished': False, 'created_on': self.get_time(), 'updated_on': self.get_time()})
+
+    def update_dl_url_to_finished(self, url):
+        return self.conn.hmset('downloads:'+url, {'finished':True, 'updated_on': self.get_time()})
+
+    def get_time(self):
+        return time.strftime('%x %X %Z')
