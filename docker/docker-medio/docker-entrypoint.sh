9b4c7809421ba1ece3ff4c950d2a38682f6e204f
treylitefm <griffin.omar@gmail.com>
2016-09-26 00:22:13 -0700

    Add wait for redis
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

---
+++ docker/docker-medio/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+nc -vz $1 $2
+
+while [ $? -eq 1 ]; do
+    echo Database not ready for connections
+    echo Sleeping 3 seconds then trying again...
+    sleep 3
+    nc -vz $1 $2
+done
+
+echo Database is ready bub
+
+python configure.py $1 $2
+/usr/bin/supervisord -n -c /var/medio/medio/flask/supervisord.conf
--- docker/docker-medio/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python configure.py $1 $2
-/usr/bin/supervisord -n -c /var/medio/medio/flask/supervisord.conf
